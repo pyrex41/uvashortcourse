5c0076577aa3a369ff4d81ef8698919eb68bfa01
pyrex41 <me@notreal.com>
2020-01-16 13:54:27 -0500

    remove:

--- materials2/course/00begin.R
+++
@@ -1,7 +0,0 @@
-install.packages("stringi") # For text manipulation
-install.packages("tm") # Framework for text mining
-install.packages("dplyr") # Data preparation and pipes $>$
-install.packages("ggplot2") # for plotting word frequencies
-install.packages("wordcloud") # wordclouds!
-install.packages("tidytext") # A tidy approach to text mining
-install.packages("stm") # For structural topic models
